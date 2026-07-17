c1cbb5726f3db4193033757717bb3c5d9cac51e7
pavan <rekapallipavan83@gmail.com>
2026-07-17 10:40:36 +0530

    removed

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import Home from './home/Home'
-
+ 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}>
-          <Route index element={<Home />} />
+          
         </Route>
       </Routes>
     </BrowserRouter>
