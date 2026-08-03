a5e76b5404d7719982d522ca0a8d85e647674a34
pavan <rekapallipavan83@gmail.com>
2026-08-03 13:19:06 +0530

    home

---
+++ src/home/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className='container py-4'>
+      <h1 className='h3 mb-3'>Welcome to SMART TECHIES HUB</h1>
+      <p className='text-muted'>This is the home page.</p>
+    </div>
+  )
+}
+
+export default Home
